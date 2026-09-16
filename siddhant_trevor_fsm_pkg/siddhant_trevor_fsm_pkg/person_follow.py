d413c2cf33663852f3215f41d2886831be74f0e0
Glorb456 <glorb458@gmail.com>
2026-09-16 15:32:30 -0400

    deleted phantom repo

--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/person_follow.py
+++
@@ -1,8 +0,0 @@
-import rclpy
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Bool
-
-class PersonFollowNode:
-    def __init__(self):
-        super().__init__('person_follow_node')
--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/wall_follow.py
+++
@@ -1,8 +0,0 @@
-import rclpy
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Bool
-
-class WallFollowNode:
-    def __init__(self):
-        super().__init__('wall_follow_node')
