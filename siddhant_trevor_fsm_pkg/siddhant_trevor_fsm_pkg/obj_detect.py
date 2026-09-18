0dda930c037b0ae8d167aadacaa93f0c9faaf601
QuixoticReconciling <trevor.e.mcdonald@gmail.com>
2026-09-17 21:32:51 -0400

    gah

--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/obj_detect.py
+++ siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/obj_detect.py
@@ -1,36 +1,36 @@
 import rclpy
 from rclpy.node import Node
+from neato2_interfaces.msg import Bump
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
 
-class ObjDetect(Node):
+class ObjDetectNode(Node):
     def __init__(self):
         super().__init__('obj_detect_node')
         self.create_timer(0.1, self.run_loop)
-        self.sub = self.create_subscription(LaserScan, 'scan', self.process_scan, 10)
+        self.bump_state = False
+        self.sub = self.create_subscription(Bump, 'bump', self.process_bump, 10)
         self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.distance_to_obstacle = None
-        self.target_distance = 0.5
 
     def run_loop(self):
         msg = Twist()
-        print(range)
-        if self.distance_to_obstacle is None:
-            msg.linear.x = 0.1
-        else:
+        print(f"Current bump state: {self.bump_state}")
+        if self.bump_state == True:
             msg.linear.x = 0.0
-            print(f"velocity is currently set to {msg.linear.x} and {msg.linear.y}")
+            print("Stopping due to bump sensor activation.")
+        else:
+            msg.linear.x = 0.3
 
         self.pub.publish(msg)
 
-    def process_scan(self, msg):
-        if msg.ranges[0] < self.target_distance:
-            self.distance_to_obstacle = msg.ranges[0]
-            print(f"Distance to obstacle: {self.distance_to_obstacle}")
+    def process_bump(self, msg):
+
+        self.bump_state = msg.left_front == 1 or msg.right_front == 1 or msg.left_rear == 1 or msg.right_rear == 1
+        print("Bump detected! Stopping the robot.")
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ObjDetect()
+    node = StopOnBumpNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
