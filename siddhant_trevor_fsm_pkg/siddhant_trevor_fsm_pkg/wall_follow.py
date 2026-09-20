19a0e958523e859ad113b90c6efb29adc1139814
QuixoticReconciling <trevor.e.mcdonald@gmail.com>
2026-09-20 18:11:34 -0400

    In progress wall_follow

--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/wall_follow.py
+++ siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/wall_follow.py
@@ -11,23 +11,46 @@ class WallFollowNode(Node):
         self.sub = self.create_subscription(LaserScan, 'scan', self.detect_error, 10)
         self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
         self.create_timer(0.1, self.run_loop)
+        self.turn_state = 0
+        print("Initializing")
 
 
     def run_loop(self):
-        self.detect_error()
+        msg = Twist()
+        if self.turn_state == 1:
+            msg.linear.x = 0.1
+            msg.angular.z = -(10*math.pi/180)
+            # print(f"Turning left at negative {msg.angular.z} rad/s")
+        elif self.turn_state == 2:
+            msg.linear.x = 0.1
+            msg.angular.z = 10*math.pi/180
+            # print(f"Turning left at {msg.angular.z} rad/s")
+        elif self.turn_state == 0:
+            msg.linear.x = 0.1
+            # print(f"Moving forward at {msg.linear.x} m/s")
+
+        self.pub.publish(msg)
     
     def detect_error(self, msg) :
-        out = Twist()
-        dist = 0.5
-        pos = msg.ranges[270]
-        if pos < dist:
-            out.linear.x = 0.1
-            out.angular.z = 10*math.pi/180
-        elif pos > dist:
-            out.linear.x = 0.1
-            out.angular.z = -(10*math.pi/180)
+        #print(f"turn state is {self.turn_state}")
+        min_dist = msg.ranges[0]
+        #min_dist_idx = 0
+        for idx, distance in enumerate(msg.ranges):
+            if distance < min_dist:
+                min_dist = distance
+                #min_dist_idx = idx
+        pos = min_dist
+        const_dist = 0.25
+
+        print(f"Distance from wall: {pos}")
+        print(f"Min dist: {pos}")
+
+        if pos < const_dist:
+            self.turn_state = 1
+        elif pos > const_dist:
+            self.turn_state = 2
         else:
-            out.lienar.x = 0.1
+            self.turn_state = 0
 
 def main(args=None):
     rclpy.init(args=args)
