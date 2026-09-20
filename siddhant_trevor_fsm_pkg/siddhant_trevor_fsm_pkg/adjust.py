2981c35e57e39279f52c7cbec74c8687be9c72ec
Glorb456 <glorb458@gmail.com>
2026-09-20 19:10:56 -0400

    working on adjust

--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/adjust.py
+++ siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/adjust.py
@@ -3,6 +3,6 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 
-class PersonFollowNode:
+class Adjust:
     def __init__(self):
         super().__init__('person_follow_node')
