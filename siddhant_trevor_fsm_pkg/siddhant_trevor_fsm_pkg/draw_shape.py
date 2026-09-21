6beca7e4e1bfa65bf54fc9829873209abb35bc57
Glorb456 <glorb458@gmail.com>
2026-09-21 16:05:21 -0400

    implemented multi-node functionality

--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/draw_shape.py
+++ siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/draw_shape.py
@@ -21,9 +21,9 @@ class DrawShape(Node):
 
     def __init__(self):
         super().__init__('draw_shape_node')
-        # create a thread to handle long-running component
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        #self.hit_pub = self.create_publisher(String, 'found_object', 10)
+        self.behavior_pub = self.create_publisher(String, 'behavior', 10)
+        self.create_subscription(String, 'behavior', self.process_behavior, 10)
         self.create_subscription(Bump, 'bump', self.process_bump, 10)
 
         self.num_turns = 5
@@ -34,48 +34,90 @@ class DrawShape(Node):
         self.bump_state = False
         self.bump = Event()
 
+
+        self.active = True
+        self.start_new_star = True
+
         self.run_loop_thread = Thread(target=self.run_loop)
         self.run_loop_thread.start()
-        self.active = True
-        self.next_node = False
 
         print("Initializing")
 
     def run_loop(self):
-        print("Begin")
+        print("Running star loop")
+        sleep(2)
+        while True:
+            if self.active and self.start_new_star:
+                self.start_new_star = False
+                self.draw_star()
+            sleep(.1)
+
+
+            
+    def stop(self):
+        self.vel_pub.publish(Twist())
+
+    def draw_star(self):
+        self.bump = Event()
         for _ in range(self.num_turns):
             if not self.bump.is_set():
                 print("running")
                 self.drive_forward()
             if not self.bump.is_set():
                 self.turn_left(-144)
-        self.stop()
-            
-    def stop(self):
-        self.vel_pub.publish(Twist())        
+
+        if not self.bump.is_set():
+            self.stop()
 
 
     def process_bump(self, msg):
-        self.bump_state = msg.left_front == 1 or msg.right_front == 1 or msg.left_rear == 1 or msg.right_rear == 1
+        self.bump_state = msg.left_front == 1 or msg.right_front == 1 or msg.left_side == 1 or msg.right_side == 1
+        if self.active == False:
+            return
         if self.bump_state:
             print("Bump detected! Stopping the neato.")
             self.bump.set()
-            #self.hit_pub.publish('found')
-            self.stop()
+            self.hand_off('DETECT_WALL')
+
+    def process_behavior(self, msg):
+        if msg.data == 'DRAW_SHAPE':
+            if self.active == False:
+                self.distance = self.distance * 1.5
+                self.time_to_drive = self.distance / self.linear_speed
+                self.start_new_star = True
+            self.active = True
+        else: 
+            self.active = False
+
+    def hand_off(self, next_name):
+        self.vel_pub.publish(Twist())
+        self.active = False
+        state_msg = String()
+        state_msg.data = next_name
+        self.behavior_pub.publish(state_msg)
+        print(f"handing off to {next_name}")
 
 
     def drive_forward(self):
         msg = Twist()
         msg.linear.x = self.linear_speed
         self.vel_pub.publish(msg)
-        sleep(self.time_to_drive)
+        steps = int(self.time_to_drive/.1)
+        for _ in range(steps):
+            if self.bump.is_set():
+                return
+            sleep(.1)
 
     def turn_left(self, angle):
         msg = Twist()
         angle_rad = angle * math.pi/180
         msg.angular.z = (angle_rad) / self.time_to_drive
         self.vel_pub.publish(msg)
-        sleep(self.time_to_drive)
+        steps = int(self.time_to_drive/.1)
+        for _ in range(steps):
+            if self.bump.is_set():
+                return
+            sleep(.1)
 
 def main(args=None):
     rclpy.init(args=args)
