348e6bd8289245dec65f5e1071a01bab6ab908ab
QuixoticReconciling <trevor.e.mcdonald@gmail.com>
2026-09-21 15:34:40 -0400

    detect_wall.py is finished but not tested

--- siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/detect_wall.py
+++ siddhant_trevor_fsm_pkg/siddhant_trevor_fsm_pkg/detect_wall.py
@@ -3,21 +3,75 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 import math
+from std_msgs.msg import String
 
 class DetectWall(Node):
+
+    
+
     def __init__(self):
         super().__init__('detect_wall_node')
+        self.create_timer(0.1, self.run_loop)
         self.sub = self.create_subscription(LaserScan, 'scan', self.detect_the_wall, 10)
         self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        self.behavior_pub = self.create_publisher(String, 'behavior', 10)
+        self.create_subscription(String, 'behavior', self.process_behavior, 10)
+        self.state = 'detecting'
+        self.is_wall = False
+        self.active = True
+        self.ticks = 0
+        self.is_adjusted = False
 
 
         print("I'm initializing")
 
+    def run_loop(self):
+        if self.active == False:
+            return
+        msg = Twist()
+        if self.state == 'detecting':
+            pass
+        elif self.state == 'adjust':
+            if self.is_adjusted:
+                self.hand_off('WALL_FOLLOW')
+                return
+            msg.angular.z = 0.0
+        elif self.state == 'backup':
+            msg.linear.x = -0.1
+            self.ticks = self.ticks + 1
+            if self.ticks >= 15:
+                self.ticks = 0
+                self.state = 'turn_around'
+        elif self.state == 'turn_around':
+            msg.angular.z = 30 * math.pi /180
+            self.ticks = self.ticks + 1
+        if self.ticks >= 60:
+            self.hand_off('DRAW_SHAPE')
+            return
+
+        self.pub.publish(msg)
+        
+    def process_scan(self, msg):
+        if self.active == False:
+            return
+        if self.state == 'classify':
+            self.detect_the_wall(msg)
+            if self.is_wall:
+                self.state = 'adjust'
+            else:
+                self.state = 'backup'
+        elif self.state == 'adjust':
+            self.adjust_neato(msg)
+
+
     def detect_the_wall(self, msg):
+        if self.active == False:
+            return
         error = .1
         min_dist = msg.ranges[0]
         min_dist_idx = 0
-        self.get_logger().info("I'm running")
+        print("I'm running")
         for idx, distance in enumerate(msg.ranges):
             if distance < min_dist:
                 min_dist = distance
@@ -34,11 +88,10 @@ class DetectWall(Node):
             
         if math.fabs(msg.ranges[minus45] - msg.ranges[plus45]) < error:
             print("WALL YEYAY")
+            self.is_wall = True
         else:
             print("NO WALL SAD")
-
-        self.spin()
-        self.adjust_neato()
+            self.is_wall = False
 
     def adjust_neato(self, msg):
         min_dist = msg.ranges[0]
@@ -48,19 +101,8 @@ class DetectWall(Node):
                 min_dist = distance
                 min_dist_idx = idx
 
-        if math.fabs(min_dist_idx - 90) < 3:
-            self.is_adjusted = True
-
-    def spin(self):
-        msg = Twist()
-        if self.is_adjusted: 
-            msg.angular.z = 0
-            self.active = False
-        else: 
-            msg.angular.z = 10 * math.pi / 180.5
-
-            
-
+        if math.fabs(min_dist_idx - 90) < 15:
+            self.is_adjusted = True          
 
 def main(args=None):
     rclpy.init(args=args)
